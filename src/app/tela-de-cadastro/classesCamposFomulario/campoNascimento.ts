031bc700331ac097e75e343951467c5dfafe1dfc
vicente L <leoratovicente@gmail.com>
2021-09-10 21:38:19 -0300

    VERSAO FINAL

--- src/app/tela-de-cadastro/classesCamposFomulario/campoNascimento.ts
+++ src/app/tela-de-cadastro/classesCamposFomulario/campoNascimento.ts
@@ -4,11 +4,26 @@
     }
 
     public static valida(nascimento: string): boolean {
-        if (nascimento =="1941-02-30" || nascimento ==""){
+        if (nascimento.length!=10){
             return false;
         }
-        else {
+        
+
+        var Ano: number = +nascimento.substring(0, 4);
+        let Mes: number = +nascimento.substring(5, 7);
+        let Dia: number = +nascimento.substring(8, 10);
+
+        if (Mes == 1 || Mes == 3 || Mes == 5 || Mes == 7 || Mes == 8 || Mes == 10 || Mes == 12){
             return true;
+        }
+        else if ( Mes == 2 && ( (Dia == 29 && (Ano%4)==0 ) || Dia<=28) ){
+            return true;
+        }
+        else if (Dia<=30){
+            return true;
+        }
+        else {
+            return false;
         }
     }
 }
